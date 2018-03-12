ea7c6333dc669d8387f65be5bedb9a8ede5e2210
punith300i <punith300i@gmail.com>
2018-03-13 00:50:03 +0530

    Add delete script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -8,22 +8,22 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
 
     // deleteMany
 
-    // db.collection('Todos').deleteMany({text:"Eat Lunch"}).then((results)=>{
-    //     console.log(results);
-    // });
+    db.collection('Todos').deleteMany({text:"Eat Lunch"}).then((results)=>{
+        console.log(results);
+    });
 
 
     // deleteOne
 
-    // db.collection('Todos').deleteOne({text:"Something to do"}).then((result)=>{
-    //     console.log(result);
-    // });
+    db.collection('Todos').deleteOne({text:"Something to do"}).then((result)=>{
+        console.log(result);
+    });
     
     
     //findOneAndDelete , returns the deleted document.
-    // db.collection('Todos').findOneAndDelete({text:"Something to do"}).then((result)=>{
-    //         console.log(result);
-    //     });
+    db.collection('Todos').findOneAndDelete({text:"Something to do"}).then((result)=>{
+            console.log(result);
+        });
    
     client.close();
 });
